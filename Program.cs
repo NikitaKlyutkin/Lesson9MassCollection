01fbc08b70157c1366cb62a247f478b5e150acc4
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-03 13:42:51 +0300

    Init

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Lesson9MassCollection;
 
 Collection<int> collection = new Collection<int>();
+collection.ListCollection = new List<int>();
 int[] nums2 = new int[4] { 1, 2, 3, 5 };
 collection.Add(nums2);
+var elem2 = nums2[2];
+Console.WriteLine(elem2);
