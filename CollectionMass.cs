d552bddf6d2aa3b9e1cc4306078de3c8f620d647
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-11-03 13:38:16 +0300

    Add project files.

---
+++ CollectionMass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9MassCollection
+{
+    internal class Collection<T>
+    {
+        public List<T> ListCollection { get; set; }
+        public void Add(T[] array) // Добавление элементов
+        {
+            ListCollection.AddRange(array);
+        }
+        public void Remove(T item) // удаление элемента
+        {
+            ListCollection.Remove(item);
+        }
+        public T GetItem(int index)
+        {
+            return ListCollection[index];
+        }
+        public T this[int index] // получение элемента
+        {
+            get => ListCollection[index];
+        }
+        public int Count() // получение длинны
+        {
+            return ListCollection.Count();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Lesson9MassCollection;
+
+Collection<int> collection = new Collection<int>();
+int[] nums2 = new int[4] { 1, 2, 3, 5 };
+collection.Add(nums2);
